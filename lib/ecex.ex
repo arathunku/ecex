9777c69ee4818b46cf6c10c34265c4cecdbfa57c
arathunku <m@e.4is.io>
2018-05-31 16:00:55 +0200

    stuff

---
+++ lib/ecex.ex
@@ -0,0 +1,18 @@
+defmodule Ecex do
+  @moduledoc """
+  Documentation for Ecex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ecex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecex_test.exs
@@ -0,0 +1,8 @@
+defmodule EcexTest do
+  use ExUnit.Case
+  doctest Ecex
+
+  test "greets the world" do
+    assert Ecex.hello() == :world
+  end
+end
